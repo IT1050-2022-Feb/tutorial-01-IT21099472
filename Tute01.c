5c6f3e5eabd6947783d480468aded0949423d654
Al balushi O. T. M. G <99330403+IT21099472@users.noreply.github.com>
2022-02-21 15:09:29 +0000

    first question completed on first tutorial.

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int a,b,total;
+  float avg;
+
+  printf("input the 1st number: ");
+  scanf("%d",&a);
+puts("");
+  printf("input the 2nd number: ");
+  scanf("%d",&b);
+
+  total=a+b;
+  avg=total/2;
+
+  printf("the Average of %f",avg);
   return 0;
 }
 
